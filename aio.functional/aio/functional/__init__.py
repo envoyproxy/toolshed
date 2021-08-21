a519a485e138ca9d0a159dd2745e387a98847e01
Ryan Northey <ryan@synca.io>
2021-08-21 16:11:35 +0100

    libs: Add aio.functional

---
+++ aio.functional/aio/functional/__init__.py
@@ -0,0 +1,5 @@
+
+from .decorators import async_property
+
+
+__all__ = ("async_property", )
---
+++ aio.functional/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+import codecs
+from setuptools import find_namespace_packages, setup  # type:ignore
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='aio.functional',
+    version='0.0.1',
+    author='Ryan Northey',
+    author_email='ryan@synca.io',
+    maintainer='Ryan Northey',
+    maintainer_email='ryan@synca.io',
+    license='Apache Software License 2.0',
+    url='https://github.com/phlax/pytooling/aio.functional',
+    description='A collection of functional utils for asyncio',
+    long_description=read('README.rst'),
+    py_modules=['aio.functional'],
+    packages=find_namespace_packages(),
+    python_requires='>=3.5',
+    extras_require={
+        "test": ["pytest", "pytest-asyncio"],
+        "lint": ['flake8'],
+        "types": ['mypy'],
+        "publish": ['wheel'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
