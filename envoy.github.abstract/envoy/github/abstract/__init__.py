78a5a65119d6c6c0ac90f62be1aaa944dbdf43f0
Ryan Northey <ryan@synca.io>
2021-08-25 08:40:53 +0100

    envoy.github.abstract: Fix imports
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- envoy.github.abstract/envoy/github/abstract/__init__.py
+++ envoy.github.abstract/envoy/github/abstract/__init__.py
@@ -2,10 +2,14 @@
 from .abstract import (
     AGithubRelease,
     AGithubReleaseAssets,
+    AGithubReleaseAssetsPusher,
+    AGithubReleaseAssetsFetcher,
     AGithubReleaseManager)
 
 
 __all__ = (
     "AGithubRelease",
     "AGithubReleaseAssets",
+    "AGithubReleaseAssetsPusher",
+    "AGithubReleaseAssetsFetcher",
     "AGithubReleaseManager")
