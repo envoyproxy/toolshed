7b6f6809f9b54333750228c9979d1ac2a48e0363
Ryan Northey <ryan@synca.io>
2021-08-29 10:44:40 +0100

    envoy.github.abstract: Add abstract command
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ envoy.github.abstract/envoy/github/abstract/command.py
@@ -0,0 +1,60 @@
+
+import argparse
+from functools import cached_property
+from typing import Dict, Optional, List
+
+import abstracts
+
+from envoy.abstract import command
+
+from .release import AGithubRelease
+from .runner import AGithubReleaseRunner
+from .manager import AGithubReleaseManager
+
+
+class AGithubReleaseCommand(
+        command.AAsyncCommand,
+        metaclass=abstracts.Abstraction):
+
+    @cached_property
+    def manager(self) -> AGithubReleaseManager:
+        return self.runner.release_manager
+
+    @cached_property
+    def parser(self) -> argparse.ArgumentParser:
+        return super().parser
+
+    @cached_property
+    def release(self) -> AGithubRelease:
+        return self.manager[self.version]
+
+    @property
+    def runner(self) -> AGithubReleaseRunner:
+        return self.context
+
+    @property
+    def version(self) -> str:
+        return self.args.version
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument("version", help="Github release version")
+
+    def format_response(
+            self,
+            release: Optional[Dict] = None,
+            assets: Optional[List[Dict]] = None,
+            errors: Optional[List[Dict]] = None) -> str:
+        for k, v in (release or {}).items():
+            if isinstance(v, dict):
+                print(k)
+                for _k, _v in v.items():
+                    _k = f"{k}.{_k}"
+                    print('{0:<30} {1}'.format(_k, _v or ""))
+                continue
+            if isinstance(v, list):
+                continue
+            print('{0:<30} {1}'.format(k, v or ""))
+        for i, result in enumerate(assets or []):
+            k = "assets" if i == 0 else ""
+            print('{0:<30} {1:<30} {2}'.format(
+                k, result["name"], result["url"]))
