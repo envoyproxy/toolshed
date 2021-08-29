768033193158d9169a20b7258e20038957d6cba0
phlax <phlax@users.noreply.github.com>
2021-08-29 16:55:52 +0100

    envoy.distribution.release: Cleanups, tests and refactoring (#59)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- envoy.distribution.release/envoy/distribution/release/runner.py
+++ envoy.distribution.release/envoy/distribution/release/runner.py
@@ -1,159 +1,49 @@
 
 import argparse
-import pathlib
-import tarfile
 from functools import cached_property
-from typing import Dict, Optional, Pattern, Type
+from typing import Optional, Type
 
 import gidgethub
 import gidgethub.abc
 
-from envoy.base import command, runner
-from envoy.github.release import manager as github_release
+import abstracts
+
+from envoy.abstract.command import AAsyncCommand
+from envoy.base import runner
+from envoy.github.abstract import (
+    GithubReleaseError, AGithubReleaseRunner, AGithubReleaseManager)
+from envoy.github.release import manager
 
 
-class BaseReleaseRunner(command.AsyncRunnerWithCommands):
-
-    @property
-    def asset_types(self) -> Optional[Dict[str, Pattern[str]]]:
-        return None
-
-    @property
-    def continues(self) -> bool:
-        return getattr(self.args, "continue")
-
-    @cached_property
-    def create_releases(self) -> bool:
-        return False
-
-    @property
-    def github(self) -> Optional[gidgethub.abc.GitHubAPI]:
-        return None
-
-    @property
-    def oauth_token(self) -> str:
-        if not self.oauth_token_file:
-            return ""
-        if not self.oauth_token_file.exists():
-            return ""
-        return self.oauth_token_file.read_text().strip()
-
-    @property
-    def oauth_token_file(self) -> Optional[pathlib.Path]:
-        if not getattr(self.args, "oauth_token_file", None):
-            return None
-        return pathlib.Path(self.args.oauth_token_file)
-
-    @property
-    def path(self) -> pathlib.Path:
-        return pathlib.Path(self.tempdir.name)
-
-    @property
-    def release_config(self) -> Dict[str, Optional[Dict[str, list]]]:
-        raise NotImplementedError
-
-    @property
-    def release_manager_class(
-            self) -> Type[github_release.GithubReleaseManager]:
-        return github_release.GithubReleaseManager
-
-    @cached_property
-    def release_managers(
-            self) -> Dict[str, github_release.GithubReleaseManager]:
-        return {
-            version: self.release_manager_class(
-                self.path,
-                self.repository,
-                version=version,
-                log=self.log,
-                create=self.create_releases,
-                github=self.github,
-                user=self.user,
-                oauth_token=self.oauth_token,
-                asset_types=self.asset_types,
-                continues=self.continues)
-            for version in self.release_config}
-
-    @cached_property
-    def release_manager(self) -> github_release.GithubReleaseManager:
-        return self.release_manager_class(
-            self.path,
-            self.repository,
-            log=self.log,
-            create=self.create_releases,
-            github=self.github,
-            user=self.user,
-            oauth_token=self.oauth_token,
-            asset_types=self.asset_types,
-            continues=self.continues)
-
-    @property
-    def repository(self) -> str:
-        return self.args.repository
-
-    @property
-    def user(self) -> str:
-        return getattr(self.args, "user", "foo")
-
-    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
-        super().add_arguments(parser)
-        parser.add_argument(
-            "repository",
-            help="Github repository")
-        parser.add_argument(
-            "oauth_token_file",
-            help="Path to an OAuth token credentials file")
-        parser.add_argument(
-            "--continue",
-            action="store_true",
-            help="Continue if an indidividual github action fails")
-
-    async def cleanup(self) -> None:
-        if "release_manager" in self.__dict__:
-            await self.release_manager.close()
-            del self.__dict__["release_manager"]
-        if "release_managers" in self.__dict__:
-            for manager in self.release_managers.values():
-                await manager.close()
-        await super().cleanup()
-
-    def create_archive(self, *paths) -> None:
-        if not self.args.archive:
-            return
-        with tarfile.open(self.args.archive, "w") as tar:
-            for path in paths:
-                if path:
-                    tar.add(path, arcname=".")
-
-
-class ReleaseRunner(BaseReleaseRunner):
-    """This runner interacts with the Github release API to build
-    distribution repositories (eg apt/yum).
+@abstracts.implementer(AGithubReleaseRunner)
+class ReleaseRunner:
+    """This runner interacts with the Github release API to create, push,
+    and fetch releases and release assets.
     """
 
     @cached_property
-    def release_config(self) -> Dict[str, Optional[Dict[str, list]]]:
-        if self.version:
-            return {version: None for version in self.version}
-        return {}
+    def command(self) -> AAsyncCommand:
+        return super().command
 
     @cached_property
-    def version(self) -> str:
-        return self.args.version
+    def commands(self) -> runner.abstract.AsyncCommandDict:
+        return super().commands
+
+    @cached_property
+    def release_manager(self) -> AGithubReleaseManager:
+        return super().release_manager
+
+    @property
+    def release_manager_class(self) -> Type[AGithubReleaseManager]:
+        return manager.GithubReleaseManager
 
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
         super().add_arguments(parser)
-        parser.add_argument(
-            "command",
-            choices=self.commands.keys(),
-            help="Command to run")
-        # parser.add_argument(
-        #   "version", nargs="*", help="Version to specify for some commands")
 
     @runner.cleansup
     @runner.catches(
         (gidgethub.GitHubException,
-         github_release.GithubReleaseError,
+         GithubReleaseError,
          KeyboardInterrupt))
     async def run(self) -> Optional[int]:
-        await super().run()
+        return await super().run()
--- envoy.distribution.release/tests/test_runner.py
+++ envoy.distribution.release/tests/test_runner.py
@@ -1,88 +1,54 @@
 
-from unittest.mock import MagicMock, PropertyMock
+from unittest.mock import AsyncMock, PropertyMock
 
 import pytest
 
 from envoy.distribution.release import runner
+from envoy.github.abstract import AGithubReleaseRunner
+from envoy.github.release.manager import GithubReleaseManager
 
 
-def _release_arg_props(patches, prop, arg=None):
+def test_runner_constructor():
+    run = runner.ReleaseRunner()
+    isinstance(run, AGithubReleaseRunner)
+    assert run.release_manager_class == GithubReleaseManager
+
+
+@pytest.mark.parametrize("prop", ["command", "commands", "release_manager"])
+def test_runner_super_props(patches, prop):
     run = runner.ReleaseRunner()
     patched = patches(
-        ("ReleaseRunner.args", dict(new_callable=PropertyMock)),
+        (f"AGithubReleaseRunner.{prop}",
+         dict(new_callable=PropertyMock)),
         prefix="envoy.distribution.release.runner")
 
-    with patched as (m_args, ):
-        assert getattr(run, prop) == getattr(m_args.return_value, arg or prop)
+    with patched as (m_prop, ):
+        assert getattr(run, prop) == m_prop.return_value
 
-
-@pytest.mark.parametrize(
-    "props",
-    (("continues", "continue"),
-     ("repository", ),
-     ("user", )))
-def test_runner_arg_props(patches, props):
-    _release_arg_props(patches, *props)
-
-
-def test_runner_oauth_token(patches):
-    run = runner.ReleaseRunner()
-    patched = patches(
-        ("ReleaseRunner.oauth_token_file", dict(new_callable=PropertyMock)),
-        prefix="envoy.distribution.release.runner")
-
-    with patched as (m_file, ):
-        assert (
-            run.oauth_token
-            == m_file.return_value.read_text.return_value.strip.return_value)
-
-    assert (
-        list(m_file.return_value.read_text.call_args)
-        == [(), {}])
-    assert (
-        list(m_file.return_value.read_text.return_value.strip.call_args)
-        == [(), {}])
-    assert "oauth_token" not in run.__dict__
-
-
-def test_runner_oauth_token_file(patches):
-    run = runner.ReleaseRunner()
-    patched = patches(
-        "pathlib",
-        ("ReleaseRunner.args", dict(new_callable=PropertyMock)),
-        prefix="envoy.distribution.release.runner")
-
-    with patched as (m_plib, m_args):
-        assert (
-            run.oauth_token_file
-            == m_plib.Path.return_value)
-
-    assert (
-        list(m_plib.Path.call_args)
-        == [(m_args.return_value.oauth_token_file, ), {}])
-    assert "oauth_token_file" not in run.__dict__
+    assert prop in run.__dict__
 
 
 def test_runner_add_arguments(patches):
     run = runner.ReleaseRunner()
-    parser = MagicMock()
     patched = patches(
-        "runner.BaseRunner.add_arguments",
+        "AGithubReleaseRunner.add_arguments",
+        prefix="envoy.distribution.release.runner")
+
+    with patched as (m_args, ):
+        assert not run.add_arguments("PARSER")
+
+    assert (
+        list(m_args.call_args)
+        == [("PARSER", ), {}])
+
+
+@pytest.mark.asyncio
+async def test_runner_run(patches):
+    run = runner.ReleaseRunner()
+    patched = patches(
+        ("AGithubReleaseRunner.run",
+         dict(new_callable=AsyncMock)),
         prefix="envoy.distribution.release.runner")
 
     with patched as (m_super, ):
-        run.add_arguments(parser)
-
-    assert (
-        list(m_super.call_args)
-        == [(parser, ), {}])
-    assert (
-        list(list(c) for c in parser.add_argument.call_args_list)
-        == [[('repository',),
-             {'help': 'Github repository'}],
-            [('oauth_token_file',),
-             {'help': 'Path to an OAuth token credentials file'}],
-            [('--continue',),
-             {'action': 'store_true',
-              'help': 'Continue if an indidividual github action fails'}],
-            [('command',), {'choices': {}.keys(), 'help': 'Command to run'}]])
+        assert await run.run() == m_super.return_value
