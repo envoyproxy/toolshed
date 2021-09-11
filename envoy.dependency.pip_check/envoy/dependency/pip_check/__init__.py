b5ccbc078a905a9828f5060f5e2c42c7e92b34bb
phlax <phlax@users.noreply.github.com>
2021-09-11 20:07:53 +0100

    envoy.dependency.pip_check: Add abstract checker (#87)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- envoy.dependency.pip_check/envoy/dependency/pip_check/__init__.py
+++ envoy.dependency.pip_check/envoy/dependency/pip_check/__init__.py
@@ -1,10 +1,12 @@
 
+from .abstract import APipChecker
 from .exceptions import PipConfigurationError
 from .checker import PipChecker
 from .cmd import cmd, main
 
 
 __all__ = (
+    "APipChecker",
     "cmd",
     "main",
     "PipConfigurationError",
