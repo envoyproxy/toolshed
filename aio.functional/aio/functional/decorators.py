4ed229a293e95bb613224e50d10e525d162d8414
phlax <phlax@users.noreply.github.com>
2022-01-14 09:35:22 +0000

    aio.functional: Fix for `async_property` (#175)
    This fixes an issue where the decorator was attaching instance to
    self.
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- aio.functional/aio/functional/decorators.py
+++ aio.functional/aio/functional/decorators.py
@@ -13,7 +13,6 @@ class NoCache(Exception):
 class async_property:  # noqa: N801
     name = None
     cache_name = "__async_prop_cache__"
-    _instance = None
 
     @classmethod
     def is_cached(cls, item: object, name: str) -> bool:
@@ -48,56 +47,54 @@ class async_property:  # noqa: N801
     def __get__(self, instance: Any, cls=None) -> Any:
         if instance is None:
             return self
-        self._instance = instance
         if inspect.isasyncgenfunction(self._fun):
-            return self.async_iter_result()
-        return self.async_result()
+            return self.async_iter_result(instance)
+        return self.async_result(instance)
 
     def fun(self, *args, **kwargs):
         if self._fun:
             return self._fun(*args, **kwargs)
 
-    @property
-    def prop_cache(self) -> dict:
-        return getattr(self._instance, self.cache_name, {})
+    def get_prop_cache(self, instance: Any) -> dict:
+        return getattr(instance, self.cache_name, {})
 
     # An async wrapper function to return the result
     # This is returned when the prop is called if the wrapped
     # method is an async generator
-    async def async_iter_result(self):
+    async def async_iter_result(self, instance: Any):
         # retrieve the value from cache if available
         try:
-            result = self.get_cached_prop()
+            result = self.get_cached_prop(instance)
         except (NoCache, KeyError):
             result = None
 
         if result is None:
-            result = self.set_prop_cache(self.fun(self._instance))
+            result = self.set_prop_cache(instance, self.fun(instance))
 
         async for item in result:
             yield item
 
     # An async wrapper function to return the result
     # This is returned when the prop is called
-    async def async_result(self) -> Any:
+    async def async_result(self, instance: Any) -> Any:
         # retrieve the value from cache if available
         try:
-            return self.get_cached_prop()
+            return self.get_cached_prop(instance)
         except (NoCache, KeyError):
             pass
 
         # derive the result, set the cache if required, and return the result
-        return self.set_prop_cache(await self.fun(self._instance))
+        return self.set_prop_cache(instance, await self.fun(instance))
 
-    def get_cached_prop(self) -> Any:
+    def get_cached_prop(self, instance: Any) -> Any:
         if not self.cache:
             raise NoCache
-        return self.prop_cache[self.name]
+        return self.get_prop_cache(instance)[self.name]
 
-    def set_prop_cache(self, result: Any) -> Any:
+    def set_prop_cache(self, instance: Any, result: Any) -> Any:
         if not self.cache:
             return result
-        cache = self.prop_cache
+        cache = self.get_prop_cache(instance)
         cache[self.name] = result
-        setattr(self._instance, self.cache_name, cache)
+        setattr(instance, self.cache_name, cache)
         return result
