d9a4775c82d35dfd99f5e6831885cbbb2004af1e
Ryan Northey <ryan@synca.io>
2022-02-10 09:11:54 +0000

    aio.core.pipe

---
+++ aio.core/aio/core/pipe/__init__.py
@@ -0,0 +1,11 @@
+
+from .abstract import AStdinStdoutProcessor
+from .pipe import StdinStdoutProcessor
+
+from . import abstract
+
+
+__all__ = (
+    "abstract"
+    "AStdinStdoutProcessor",
+    "StdinStdoutProcessor")
---
+++ aio.core/aio/core/pipe/abstract/__init__.py
@@ -0,0 +1,6 @@
+
+from .pipe import AStdinStdoutProcessor
+
+
+__all__ = (
+    "AStdinStdoutProcessor", )
