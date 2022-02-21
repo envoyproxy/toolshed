79ac58ea82daa8f185eaaa21bb9de4bd18cc6766
phlax <phlax@users.noreply.github.com>
2022-02-21 10:39:09 +0000

    `envoy.code.check`: Optimize `flake8` check (#350)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- envoy.code.check/tests/test_abstract_flake8.py
+++ envoy.code.check/tests/test_abstract_flake8.py
@@ -3,6 +3,8 @@ from unittest.mock import AsyncMock, MagicMock, PropertyMock
 
 import pytest
 
+from flake8.main.application import Application
+
 from envoy.code import check
 
 
@@ -52,8 +54,6 @@ def test_flake8_constructor():
     flake8 = check.AFlake8Check("DIRECTORY")
     assert flake8.directory == "DIRECTORY"
     assert isinstance(flake8, check.ACodeCheck)
-    assert flake8._errors == {}
-    assert "_errors" in flake8.__dict__
 
 
 @pytest.mark.parametrize("files", [True, False])
@@ -84,6 +84,31 @@ async def test_flake8_checker_files(patches, files):
         hasattr(
             flake8,
             check.AFlake8Check.checker_files.cache_name))
+
+
+async def test_flake8_errors(patches):
+    directory = MagicMock()
+    flake8 = check.AFlake8Check(directory)
+    patched = patches(
+        ("AFlake8Check.flake8_errors",
+         dict(new_callable=PropertyMock)),
+        "AFlake8Check.handle_errors",
+        prefix="envoy.code.check.abstract.flake8")
+
+    with patched as (m_errors, m_handle):
+        errors = AsyncMock()
+        m_errors.side_effect = errors
+        assert (
+            await flake8.errors
+            == m_handle.return_value)
+
+    assert (
+        m_handle.call_args
+        == [(errors.return_value, ), {}])
+    assert not (
+        hasattr(
+            flake8,
+            check.AFlake8Check.errors.cache_name))
 
 
 def test_flake8_flake8_args(patches):
@@ -118,36 +143,31 @@ async def test_flake8_flake8_errors(patches):
     directory = MagicMock()
     flake8 = check.AFlake8Check(directory)
     patched = patches(
-        "threaded",
         ("AFlake8Check.absolute_paths",
          dict(new_callable=PropertyMock)),
         ("AFlake8Check.flake8_args",
          dict(new_callable=PropertyMock)),
-        ("AFlake8Check._errors",
-         dict(new_callable=PropertyMock)),
         "AFlake8Check.check_flake8_files",
-        "AFlake8Check._handle_error",
+        "AFlake8Check.execute",
         prefix="envoy.code.check.abstract.flake8")
 
-    with patched as (m_threaded, m_abs, m_args, m_errs, m_checks, m_handle):
+    with patched as (m_abs, m_args, m_checks, m_execute):
         files = AsyncMock()
         m_abs.side_effect = files
         assert (
-            await flake8.errors
-            == m_errs.return_value)
+            await flake8.flake8_errors
+            == m_execute.return_value)
 
     assert (
-        m_threaded.call_args
+        m_execute.call_args
         == [(m_checks,
              directory.absolute_path,
              files.return_value,
-             m_args.return_value),
-            dict(stdout=m_handle)])
-
+             m_args.return_value), {}])
     assert not (
         hasattr(
             flake8,
-            check.AFlake8Check.errors.cache_name))
+            check.AFlake8Check.flake8_errors.cache_name))
 
 
 @pytest.mark.parametrize("files", [True, False])
@@ -180,55 +200,206 @@ async def test_flake8_problem_files(patches, files):
         assert not m_errors.called
 
 
-@pytest.mark.parametrize("msg", [True, False])
-async def test_flake8__handle_error(patches, msg):
+def test_flake8_handle_errors(patches):
+    flake8 = check.AFlake8Check("DIRECTORY")
+    patched = patches(
+        "AFlake8Check._parse_error",
+        prefix="envoy.code.check.abstract.flake8")
+    errors = [MagicMock() for i in range(0, 5)]
+
+    class Handler:
+        counter = 0
+
+        def _parse_error(self, error):
+            self.counter += 1
+            if self.counter % 2:
+                return "PATH1", f"MESSAGE{self.counter}"
+            return "PATH2", f"MESSAGE{self.counter}"
+
+    handler = Handler()
+
+    with patched as (m_parse, ):
+        m_parse.side_effect = handler._parse_error
+        assert (
+            flake8.handle_errors(errors)
+            == {'PATH1': ['MESSAGE1', 'MESSAGE3', 'MESSAGE5'],
+                'PATH2': ['MESSAGE2', 'MESSAGE4']})
+
+    assert (
+        m_parse.call_args_list
+        == [[(error, ),
+             {}]
+            for error in errors])
+
+
+def test_flake8__parse_error():
     directory = MagicMock()
     flake8 = check.AFlake8Check(directory)
-    patched = patches(
-        "str",
-        ("AFlake8Check._errors",
-         dict(new_callable=PropertyMock)),
-        prefix="envoy.code.check.abstract.flake8")
-
-    with patched as (m_str, m_errors):
-        if not msg:
-            m_str.return_value = None
-        assert not await flake8._handle_error("ERROR")
-
+    error = MagicMock()
     assert (
-        m_str.call_args
-        == [("ERROR", ), {}])
-    if not msg:
-        assert not directory.relative_path.called
-        return
-    message = m_str.return_value
+        flake8._parse_error(error)
+        == (directory.relative_path.return_value,
+            (f"{directory.relative_path.return_value}: "
+             f"{error.split.return_value.__getitem__.return_value}")))
     assert (
         directory.relative_path.call_args
-        == [(m_str.return_value.split.return_value.__getitem__.return_value, ),
-            {}])
-    path = directory.relative_path.return_value
+        == [(error.split.return_value.__getitem__.return_value, ), {}])
     assert (
-        m_errors.return_value.__setitem__.call_args
-        == [(path, m_errors.return_value.get.return_value, ), {}])
-    assert (
-        m_errors.return_value.get.call_args
-        == [(path, []), {}])
-    assert (
-        m_errors.return_value.__getitem__.call_args
-        == [(path, ), {}])
-    assert (
-        m_errors.return_value.__getitem__.return_value.append.call_args
-        == [(f"{path}: {message.split.return_value.__getitem__.return_value}",
-             ), {}])
-
-    assert (
-        message.split.call_args_list
+        error.split.call_args_list
         == [[(":", ), {}],
             [(":", 1), {}]])
     assert (
-        message.split.return_value.__getitem__.call_args_list
-        == [[(0, ), {}],
+        error.split.return_value.__getitem__.call_args_list
+        == [[(0,), {}],
             [(1, ), {}]])
+
+
+def test_flake8app_constructor(patches):
+    flake8app = check.abstract.flake8.Flake8App("PATH", "ARGS")
+    assert flake8app.path == "PATH"
+    assert flake8app.args == "ARGS"
+    assert flake8app._excluded_paths == set()
+    assert "_excluded_paths" in flake8app.__dict__
+
+
+def test_flake8app_app(patches):
+    flake8app = check.abstract.flake8.Flake8App("PATH", "ARGS")
+    patched = patches(
+        "Flake8Application",
+        prefix="envoy.code.check.abstract.flake8")
+
+    with patched as (m_app, ):
+        assert flake8app.app == m_app.return_value
+
+    assert (
+        m_app.call_args
+        == [(), {}])
+    assert (
+        m_app.return_value.initialize.call_args
+        == [("ARGS", ), {}])
+    assert "app" in flake8app.__dict__
+
+
+def test_flake8app_manager(patches):
+    flake8app = check.abstract.flake8.Flake8App("PATH", "ARGS")
+    patched = patches(
+        ("Flake8App.app",
+         dict(new_callable=PropertyMock)),
+        prefix="envoy.code.check.abstract.flake8")
+
+    with patched as (m_app, ):
+        assert (
+            flake8app.manager
+            == m_app.return_value.file_checker_manager)
+
+    assert "manager" not in flake8app.__dict__
+
+
+@pytest.mark.parametrize("filename_matches", [True, False])
+@pytest.mark.parametrize("include_dir", [True, False])
+@pytest.mark.parametrize("is_excluded", [True, False])
+def test_flake8app_include_file(
+        patches, filename_matches, include_dir, is_excluded):
+    path = MagicMock()
+    flake8 = check.abstract.flake8.Flake8App(path, "ARGS")
+    patched = patches(
+        "os",
+        "Flake8App._filename_matches",
+        "Flake8App._include_directory",
+        "Flake8App._is_excluded",
+        prefix="envoy.code.check.abstract.flake8")
+
+    with patched as (m_os, m_match, m_include, m_exclude):
+        m_match.return_value = filename_matches
+        m_include.return_value = include_dir
+        m_exclude.return_value = is_excluded
+        assert (
+            flake8.include_file("INCPATH")
+            == (filename_matches
+                and include_dir
+                and not is_excluded))
+
+    assert (
+        m_os.path.join.call_args
+        == [(path, "INCPATH"), {}])
+    assert (
+        m_match.call_args
+        == [(m_os.path.join.return_value, ), {}])
+    if not filename_matches:
+        assert not m_include.called
+        assert not m_exclude.called
+        assert not m_os.path.dirname.called
+        return
+    assert (
+        m_include.call_args
+        == [(m_os.path.dirname.return_value, ), {}])
+    assert (
+        m_os.path.dirname.call_args
+        == [(m_os.path.join.return_value, ), {}])
+    if not include_dir:
+        assert not m_exclude.called
+        return
+    assert (
+        m_exclude.call_args
+        == [(m_os.path.join.return_value, ), {}])
+
+
+@pytest.mark.parametrize("n", range(1, 5))
+def test_flake8app_include_files(patches, n):
+    flake8 = check.abstract.flake8.Flake8App("PATH", "ARGS")
+    patched = patches(
+        "os",
+        "Flake8App.include_file",
+        prefix="envoy.code.check.abstract.flake8")
+    files = [f"F{i}" for i in range(0, 7)]
+
+    class Includer:
+        counter = 0
+
+        def include_file(self, f):
+            self.counter += 1
+            return bool(self.counter % n)
+
+    includer = Includer()
+
+    with patched as (m_os, m_include):
+        m_include.side_effect = includer.include_file
+        assert (
+            flake8.include_files(files)
+            == set([
+                f
+                for i, f
+                in enumerate(files)
+                if (i + 1) % n]))
+
+    assert (
+        m_include.call_args_list
+        == [[(m_os.path.join.return_value, ), {}]] * 7)
+    assert (
+        m_os.path.join.call_args_list
+        == [[("PATH", f), {}]
+            for f in files])
+
+
+def test_flake8app_run_checks(patches):
+    flake8 = check.abstract.flake8.Flake8App("PATH", "ARGS")
+    patched = patches(
+        ("Flake8App.app",
+         dict(new_callable=PropertyMock)),
+        prefix="envoy.code.check.abstract.flake8")
+    paths = MagicMock()
+
+    with patched as (m_app, ):
+        assert (
+            flake8.run_checks(paths)
+            == m_app.return_value._results)
+
+    assert (
+        m_app.return_value.run_checks.call_args
+        == [(), dict(files=paths)])
+    assert (
+        m_app.return_value.report.call_args
+        == [(), {}])
 
 
 def test_flake8app__filename_matches(patches):
@@ -296,55 +467,6 @@ def test_flake8app__include_directory(patches, paths, exclude):
     assert flake8._include_directory.cache_info().currsize >= 1
 
 
-@pytest.mark.parametrize("filename_matches", [True, False])
-@pytest.mark.parametrize("include_dir", [True, False])
-@pytest.mark.parametrize("is_excluded", [True, False])
-def test_flake8app__include_file(
-        patches, filename_matches, include_dir, is_excluded):
-    path = MagicMock()
-    flake8 = check.abstract.flake8.Flake8App(path, "ARGS")
-    patched = patches(
-        "os",
-        "Flake8App._filename_matches",
-        "Flake8App._include_directory",
-        "Flake8App._is_excluded",
-        prefix="envoy.code.check.abstract.flake8")
-
-    with patched as (m_os, m_match, m_include, m_exclude):
-        m_match.return_value = filename_matches
-        m_include.return_value = include_dir
-        m_exclude.return_value = is_excluded
-        assert (
-            flake8.include_file("INCPATH")
-            == (filename_matches
-                and include_dir
-                and not is_excluded))
-
-    assert (
-        m_os.path.join.call_args
-        == [(path, "INCPATH"), {}])
-    assert (
-        m_match.call_args
-        == [(m_os.path.join.return_value, ), {}])
-    if not filename_matches:
-        assert not m_include.called
-        assert not m_exclude.called
-        assert not m_os.path.dirname.called
-        return
-    assert (
-        m_include.call_args
-        == [(m_os.path.dirname.return_value, ), {}])
-    assert (
-        m_os.path.dirname.call_args
-        == [(m_os.path.join.return_value, ), {}])
-    if not include_dir:
-        assert not m_exclude.called
-        return
-    assert (
-        m_exclude.call_args
-        == [(m_os.path.join.return_value, ), {}])
-
-
 @pytest.mark.parametrize("any_paths", [True, False])
 @pytest.mark.parametrize("is_excluded", [True, False])
 def test_flake8app__include_path(patches, any_paths, is_excluded):
@@ -394,16 +516,99 @@ def test_flake8app__include_path(patches, any_paths, is_excluded):
 def test_flake8__is_excluded(patches):
     flake8 = check.abstract.flake8.Flake8App("PATH", "ARGS")
     patched = patches(
-        ("Flake8App.app",
+        ("Flake8App.manager",
          dict(new_callable=PropertyMock)),
         prefix="envoy.code.check.abstract.flake8")
 
-    with patched as (m_app, ):
+    with patched as (m_manager, ):
         assert (
             flake8._is_excluded("EXCPATH")
-            == (m_app.return_value
-                     .file_checker_manager.is_path_excluded.return_value))
+            == (m_manager.return_value.is_path_excluded.return_value))
 
     assert (
-        m_app.return_value.file_checker_manager.is_path_excluded.call_args
+        m_manager.return_value.is_path_excluded.call_args
         == [("EXCPATH", ), {}])
+
+
+def test_flake8application_constructor():
+    app = check.abstract.flake8.Flake8Application()
+    assert isinstance(app, Application)
+
+
+def test_flake8application_output_fd(patches):
+    app = check.abstract.flake8.Flake8Application()
+    patched = patches(
+        "io",
+        prefix="envoy.code.check.abstract.flake8")
+
+    with patched as (m_io, ):
+        assert (
+            app.output_fd
+            == m_io.StringIO.return_value)
+
+    assert (
+        m_io.StringIO.call_args
+        == [(), {}])
+    assert "output_fd" in app.__dict__
+
+
+@pytest.mark.parametrize("n", range(1, 5))
+def test_flake8application__stop(patches, n):
+    app = check.abstract.flake8.Flake8Application()
+    patched = patches(
+        ("Flake8Application.output_fd",
+         dict(new_callable=PropertyMock)),
+        prefix="envoy.code.check.abstract.flake8")
+    app._formatter_stop = MagicMock()
+    results = [
+        ("F{i}"
+         if i % n
+         else None)
+        for i
+        in range(0, 7)]
+
+    class Splitter:
+
+        def split(self, f):
+            return results
+
+    splitter = Splitter()
+
+    with patched as (m_out, ):
+        (m_out.return_value.read
+              .return_value.strip
+              .return_value.split.side_effect) = splitter.split
+        assert not app._stop()
+
+    assert (
+        m_out.return_value.seek.call_args
+        == [(0, ), {}])
+    assert (
+        app._formatter_stop.call_args
+        == [(), {}])
+    assert (
+        app._results
+        == [r for r in results if r])
+
+
+def test_flake8application_make_formatter(patches):
+    app = check.abstract.flake8.Flake8Application()
+    patched = patches(
+        ("Application.make_formatter",
+         dict(new_callable=PropertyMock)),
+        ("Flake8Application.output_fd",
+         dict(new_callable=PropertyMock)),
+        "Flake8Application._stop",
+        prefix="envoy.code.check.abstract.flake8")
+    app.formatter = MagicMock()
+    stop = app.formatter.stop
+
+    with patched as (m_super, m_out, m_stop):
+        assert not app.make_formatter()
+
+    assert (
+        m_super.call_args
+        == [(), {}])
+    assert app.formatter.output_fd == m_out.return_value
+    assert app._formatter_stop == stop
+    assert app.formatter.stop == m_stop
