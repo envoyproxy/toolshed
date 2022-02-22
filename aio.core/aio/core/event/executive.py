d89afde1757e93e71f2e0c9b36d38dc3c2e95dc3
Ryan Northey <ryan@synca.io>
2022-02-22 17:29:59 +0000

    `aio.core`: Add `execute_in_batches` to `IExecutive`
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- aio.core/aio/core/event/executive.py
+++ aio.core/aio/core/event/executive.py
@@ -1,9 +1,13 @@
 
-from typing import Any
+import logging
+from typing import Any, Callable, Optional
 
 import abstracts
 
-from aio.core import event
+from aio.core import event, functional, tasks
+
+
+logger = logging.getLogger(__name__)
 
 
 # TODO: split `IReactive.pool` to here
@@ -11,7 +15,23 @@ class IExecutive(event.IReactive, metaclass=abstracts.Interface):
     """Object that executes commands in a process pool."""
 
     @abstracts.interfacemethod
-    async def execute(self, *args, **kwargs) -> Any:
+    async def execute(
+            self,
+            executable: Callable,
+            *args,
+            **kwargs) -> Any:
+        """Execute a command in a process pool."""
+        raise NotImplementedError
+
+    @abstracts.interfacemethod
+    async def execute_in_batches(
+            self,
+            executable: Callable,
+            *args,
+            concurrency: Optional[int] = None,
+            min_batch_size: Optional[int] = None,
+            max_batch_size: Optional[int] = None,
+            **kwargs) -> functional.AwaitableGenerator:
         """Execute a command in a process pool."""
         raise NotImplementedError
 
@@ -19,8 +39,34 @@ class IExecutive(event.IReactive, metaclass=abstracts.Interface):
 @abstracts.implementer((event.AReactive, IExecutive))
 class AExecutive(metaclass=abstracts.Abstraction):
 
-    async def execute(self, *args, **kwargs) -> Any:
+    async def execute(
+            self,
+            executable: Callable,
+            *args,
+            **kwargs) -> Any:
+        logger.debug(
+            f"Executing: {executable}\n  {args}\n  {kwargs}")
         return await self.loop.run_in_executor(
             self.pool,
-            *args,
+            *(executable, *args),
             **kwargs)
+
+    def execute_in_batches(
+            self,
+            executable: Callable,
+            *args,
+            concurrency: Optional[int] = None,
+            min_batch_size: Optional[int] = None,
+            max_batch_size: Optional[int] = None,
+            **kwargs) -> functional.AwaitableGenerator:
+        return tasks.concurrent(
+            (self.execute(
+                executable,
+                *batch,
+                **kwargs)
+             for batch
+             in functional.batch_jobs(
+                 args,
+                 min_batch_size=min_batch_size,
+                 max_batch_size=max_batch_size)),
+            limit=concurrency)
--- aio.core/tests/test_event_executive.py
+++ aio.core/tests/test_event_executive.py
@@ -1,5 +1,6 @@
 
-from unittest.mock import AsyncMock, PropertyMock
+import types
+from unittest.mock import AsyncMock, MagicMock, PropertyMock
 
 import pytest
 
@@ -19,8 +20,13 @@ class DummyExecutiveIface():
     def pool(self):
         return event.IExecutive.pool.fget(self)
 
-    async def execute(self, *args, **kwargs):
-        return await event.IExecutive.execute(*args, **kwargs)
+    async def execute(self, command, *args, **kwargs):
+        return await event.IExecutive.execute(
+            command, *args, **kwargs)
+
+    async def execute_in_batches(self, command, *args, **kwargs):
+        return await event.IExecutive.execute_in_batches(
+            command, *args, **kwargs)
 
 
 @abstracts.implementer(event.IExecutive)
@@ -37,9 +43,9 @@ async def test_event_executive_iface_constructor():
     for iface_prop in ["loop", "pool"]:
         with pytest.raises(NotImplementedError):
             getattr(executive, iface_prop)
-    for async_iface_method in ["execute"]:
+    for async_iface_method in ["execute", "execute_in_batches"]:
         with pytest.raises(NotImplementedError):
-            await getattr(executive, async_iface_method)()
+            await getattr(executive, async_iface_method)("EXECUTABLE")
 
 
 def test_event_executive_constructor():
@@ -63,9 +69,67 @@ async def test_event_executive_execute(patches, args, kwargs):
         execute = AsyncMock()
         m_loop.return_value.run_in_executor = execute
         assert (
-            await executive.execute(*args, **kwargs)
+            await executive.execute("EXECUTABLE", *args, **kwargs)
             == m_loop.return_value.run_in_executor.return_value)
 
     assert (
         m_loop.return_value.run_in_executor.call_args
-        == [(m_pool.return_value, *args), kwargs])
+        == [(m_pool.return_value, "EXECUTABLE", *args), kwargs])
+
+
+@pytest.mark.parametrize(
+    "args", [[], [f"ARG{i}" for i in range(0, 5)]])
+@pytest.mark.parametrize(
+    "kwargs", [{}, {f"K{i}": f"V{i}" for i in range(0, 5)}])
+@pytest.mark.parametrize("concurrency", [None, *range(0, 5)])
+@pytest.mark.parametrize("max_batch_size", [None, *range(0, 5)])
+@pytest.mark.parametrize("min_batch_size", [None, *range(0, 5)])
+async def test_event_executive_execute_in_batches(
+        patches, args, kwargs, concurrency, max_batch_size, min_batch_size):
+    executive = DummyExecutive()
+    patched = patches(
+        "functional",
+        "tasks",
+        ("AExecutive.execute",
+         dict(new_callable=MagicMock)),
+        prefix="aio.core.event.executive")
+    call_kwargs = kwargs.copy()
+    c_kwargs = {}
+    if concurrency is not None:
+        call_kwargs["concurrency"] = concurrency
+    if max_batch_size is not None:
+        call_kwargs["max_batch_size"] = max_batch_size
+    if min_batch_size is not None:
+        call_kwargs["min_batch_size"] = min_batch_size
+    c_kwargs["limit"] = concurrency
+    batches = [f"BATCH{i}" for i in range(0, 5)]
+
+    with patched as (m_func, m_tasks, m_exec):
+        concurrent = AsyncMock()
+        m_tasks.concurrent.side_effect = concurrent
+        m_func.batch_jobs.return_value = batches
+        assert (
+            await executive.execute_in_batches(
+                "EXECUTABLE",
+                *args,
+                **call_kwargs)
+            == concurrent.return_value)
+        task_iter = concurrent.call_args[0][0]
+        assert isinstance(task_iter, types.GeneratorType)
+        assert (
+            list(task_iter)
+            == [m_exec.return_value] * 5)
+
+    assert (
+        concurrent.call_args
+        == [(task_iter, ), c_kwargs])
+    assert (
+        m_func.batch_jobs.call_args
+        == [(tuple(args), ),
+            dict(max_batch_size=max_batch_size,
+                 min_batch_size=min_batch_size)])
+    assert (
+        m_exec.call_args_list
+        == [[("EXECUTABLE", *batch), kwargs]
+            for batch
+            in batches])
