02d806c0ac4e059f73b9305d6947c4fa3384f86f
Ryan Northey <ryan@synca.io>
2022-06-02 08:55:41 +0100

    `envoy.base.utils`: Minor typing cleanup
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- envoy.base.utils/envoy/base/utils/jinja_env_cmd.py
+++ envoy.base.utils/envoy/base/utils/jinja_env_cmd.py
@@ -1,14 +1,12 @@
 
 import sys
+from typing import Optional
 
 from .jinja_env import JinjaEnvironment
 
 
-def main(*args: str) -> int:
-    return (
-        0
-        if not JinjaEnvironment.create(*args)
-        else 1)
+def main(*args: str) -> Optional[int]:
+    return JinjaEnvironment.create(*args)
 
 
 def jinja_env_cmd() -> None:
