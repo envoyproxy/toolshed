bbf9cda7ef0a12d03b0afc2173a97bd9db1dd863
Ryan Northey <ryan@synca.io>
2023-10-02 18:02:31 +0100

    bazel/updater: Fix sha replacement
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- bazel/dependency/bazel-update.sh
+++ bazel/dependency/bazel-update.sh
@@ -23,6 +23,7 @@ REPO="$(${JQ} -r ".${DEP}.repo" "${DEP_DATA}")"
 URL="$(${JQ} -r ".${DEP}.url" "${DEP_DATA}")"
 DEP_SEARCH="\"${DEP}\": {"
 VERSION_SEARCH="\"version\": \"${EXISTING_VERSION}\","
+SHA_SEARCH="\"sha256\": \"${EXISTING_SHA}\","
 
 get_sha () {
     local url sha repo version url
@@ -56,9 +57,31 @@ find_version_line () {
     printf "$version_match_ln"
 }
 
+find_sha_line () {
+    # This needs to find the correct version to replace
+    match="$(\
+        grep -n "${DEP_SEARCH}" "${VERSION_FILE}" \
+        | cut -d: -f-2)"
+    match_ln="$(\
+        echo "${match}" \
+        | cut -d: -f1)"
+    match_ln="$((match_ln + 1))"
+    sha_match_ln="$(\
+        tail -n "+${match_ln}" "${VERSION_FILE}" \
+        | grep -n "${SHA_SEARCH}" \
+        | head -n1 \
+        | cut -d: -f1)"
+    sha_match_ln="$((match_ln + sha_match_ln - 1))"
+    printf "$sha_match_ln"
+}
+
 update_sha () {
-    local search="$1" replace="$2"
-    $UPDATE_TOOL ${BUILD_WORKSPACE_DIRECTORY} ${search}:${replace}
+    local match_ln search replace
+    match_ln="$1"
+    search="$2"
+    replace="$3"
+    echo "Updating sha: ${search} -> ${replace}"
+    sed -i "${match_ln}s/${search}/${replace}/" "$VERSION_FILE"
 }
 
 update_version () {
@@ -79,11 +102,16 @@ update_dependency () {
     fi
     sha="$(get_sha "${URL}" "${REPO}" "${VERSION}")"
     if [[ -z "$sha" ]]; then
+        echo "Unable to resolve sha for ${DEP}/${VERSION}" >&2
+        exit 1
+    fi
+    sha_ln="$(find_sha_line)"
+    if [[ -z "$sha_ln" ]]; then
         echo "Unable to find sha for ${DEP}/${VERSION}" >&2
         exit 1
     fi
     update_version "${dep_ln}" "${EXISTING_VERSION}" "${VERSION}"
-    update_sha "${EXISTING_SHA}" "${sha}"
+    update_sha "${sha_ln}" "${EXISTING_SHA}" "${sha}"
 }
 
 update_dependency
