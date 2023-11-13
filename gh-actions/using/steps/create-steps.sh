268d6327e5ff0f41989720a4bb1e728f0d15b2cd
Ryan Northey <ryan@synca.io>
2023-11-13 07:06:57 +0000

    `gh-actions/using/steps`: Add support for json steps
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- gh-actions/using/steps/create-steps.sh
+++ gh-actions/using/steps/create-steps.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
 mkdir -p .tmp.action
-__STEPS__=$(printf "%s\n" "${STEPS}" \
-    | sed 's/\([^\\]\)%{{ /\1${{ /g; s/^/  /')
+if [[ "$FORMAT" == "yaml" ]]; then
+    __STEPS__=$(printf "%s\n" "${STEPS}" \
+        | sed 's/\([^\\]\)%{{ /\1${{ /g; s/^/  /')
+    __STEPS__="\n${__STEPS__}"
+else
+    __STEPS__=$(printf "%s\n" "${STEPS}" \
+        | sed 's/\([^\\]\)%{{ /\1${{ /g')
+fi
 export __STEPS__
 cat "$1" | envsubst > .tmp.action/action.yml
