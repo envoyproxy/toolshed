bde321c57b9710b477825a692df867d092a6c34e
phlax <phlax@users.noreply.github.com>
2023-11-29 09:05:50 +0000

    gh-actions: Update and use node 20 (#1152)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- gh-actions/torun/__tests__/main.test.ts
+++ gh-actions/torun/__tests__/main.test.ts
@@ -6,6 +6,8 @@ beforeEach(() => {
   jest.resetModules()
   jest.spyOn(core, 'getInput').mockImplementation((name: string): string => {
     if (name === 'token') return '12345'
+    if (name === 'config') return '{}'
+    if (name === 'paths') return '{}'
     return ''
   })
 
