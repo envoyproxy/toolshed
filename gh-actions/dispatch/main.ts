1e870d7aff7eaf2f93ab8cb3efa01ea93d8095aa
Ryan Northey <ryan@synca.io>
2023-12-03 08:09:22 +0000

    `gh-actions/dispatch`: Remove appauth
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- gh-actions/dispatch/main.ts
+++ gh-actions/dispatch/main.ts
@@ -1,13 +1,8 @@
-import {createAppAuth} from '@octokit/auth-app'
 import * as core from '@actions/core'
-import {Endpoints} from '@octokit/types'
-import {Octokit} from '@octokit/rest'
 import * as github from '@actions/github'
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import * as yaml from 'js-yaml'
-
-type listInstallationsResponse = Endpoints['GET /app/installations']['response']
 
 const run = async (): Promise<void> => {
   try {
@@ -15,32 +10,11 @@ const run = async (): Promise<void> => {
     if (!repository || repository === '') return
     const ref = core.getInput('ref')
     const providedInputs = core.getInput('inputs')
-    const privateKey = core.getInput('key')
-    const appId = core.getInput('app_id')
-    let installationId = parseInt(core.getInput('installation_id'))
+    const token = core.getInput('token')
     const workflow = core.getInput('workflow')
-    const appOctokit = new Octokit({
-      authStrategy: createAppAuth,
-      auth: {
-        appId,
-        privateKey,
-      },
-      baseUrl: process.env.GITHUB_API_URL || 'https://api.github.com',
-    })
-    if (!installationId) {
-      const installations: listInstallationsResponse = await appOctokit.apps.listInstallations()
-      installationId = installations.data[0].id
-    }
-    const resp = await appOctokit.auth({
-      type: 'installation',
-      installationId,
-    })
-
     const inputs: {[key: string]: string | number | boolean} = {}
-
     if (providedInputs) {
       const parsedInputs = yaml.load(providedInputs)
-
       for (const [key, value] of Object.entries(parsedInputs)) {
         if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') {
           inputs[key] = value
@@ -49,11 +23,6 @@ const run = async (): Promise<void> => {
         }
       }
     }
-
-    // @ts-expect-error
-    if (!resp || !resp.token) {
-      throw new Error('Unable to authenticate')
-    }
     const request = {
       action: `POST /repos/${repository}/actions/workflows/${workflow}/dispatches`,
       params: {
@@ -61,8 +30,7 @@ const run = async (): Promise<void> => {
         inputs,
       },
     }
-    // @ts-expect-error
-    const octokit = github.getOctokit(resp.token)
+    const octokit = github.getOctokit(token)
     await octokit.request(request.action, request.params)
     console.log(`Dispatched ${repository}/${workflow}`)
   } catch (error) {
