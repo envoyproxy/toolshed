5d4f2c6807a7abb9897cea7850bfa9951fd3dfe8
phlax <phlax@users.noreply.github.com>
2023-12-11 15:25:47 +0000

    `gh-actions/github/checks`: Add update fun (#1252)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- gh-actions/github/checks/main.ts
+++ gh-actions/github/checks/main.ts
@@ -4,9 +4,12 @@ import {GitHub} from '@actions/github/lib/utils'
 import {RequestParameters} from '@octokit/types'
 
 interface Check {
-  action: string
+  action?: string
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  head_sha?: string
   id: string
   name: string
+  output?: CheckOutput
 }
 
 interface CheckMap {
@@ -35,26 +38,77 @@ async function createCheckRun(octokit: OctokitType, checkRun: RequestParameters)
   return [checkRun.id as string, response.data.id]
 }
 
+async function startChecks(
+  octokit: OctokitType,
+  checks: CheckMap,
+  config: CheckConfigs,
+  textExtra: string,
+): Promise<void> {
+  const nwo = process.env['GITHUB_REPOSITORY'] || '/'
+  const [owner, repo] = nwo.split('/')
+  const requests: Promise<[string, number]>[] = []
+  Object.entries(checks).forEach(([check, checkConfig]) => {
+    const name = checkConfig.name
+    const id = check
+    const checkRequestConfig: CheckConfig = checkConfig.action === 'SKIP' ? {...config.skipped} : {...config.run}
+    const output = {...checkRequestConfig.output}
+    output.text = textExtra === '' ? output.text : `${checkRequestConfig.output.text}\n${textExtra}`
+    requests.push(createCheckRun(octokit, {...checkRequestConfig, id, name, owner, repo, output}))
+  })
+  const checkRunIds: [string, number][] = await Promise.all(requests)
+  core.setOutput('checks', JSON.stringify(Object.fromEntries(checkRunIds), null, 0))
+}
+
+async function updateCheckRun(octokit: OctokitType, checkConfig: Check, textExtra: string): Promise<[string, number]> {
+  const nwo = process.env['GITHUB_REPOSITORY'] || '/'
+  const [owner, repo] = nwo.split('/')
+  const output = checkConfig.output || {text: ''}
+  output.text = textExtra === '' ? output.text : `${output.text}\n${textExtra}`
+  const checkResponse = await octokit.rest.checks.listForRef({
+    owner,
+    repo,
+    ref: checkConfig.head_sha,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    check_name: checkConfig.name,
+    filter: 'latest',
+  })
+  const text = checkResponse.data.check_runs[0].output.text
+  checkConfig.id = checkResponse.data.check_runs[0].id
+  output.text = `${output.text}\n### Check started by\n${text.split('### Check started by')[1]}`
+  checkConfig.output = output
+  const response = await octokit.rest.checks.update({
+    owner,
+    repo,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    check_run_id: checkConfig.id,
+    ...checkConfig,
+  })
+  console.log(JSON.stringify(response))
+  return [checkConfig.name, response.data.id]
+}
+
+async function updateChecks(octokit: OctokitType, checks: CheckMap, textExtra: string): Promise<void> {
+  const requests: Promise<[string, number]>[] = []
+  Object.values(checks).forEach(checkConfig => {
+    requests.push(updateCheckRun(octokit, checkConfig, textExtra))
+  })
+  const checkRunIds: [string, number][] = await Promise.all(requests)
+  core.setOutput('checks', JSON.stringify(Object.fromEntries(checkRunIds), null, 0))
+}
+
 const run = async (): Promise<void> => {
   try {
-    const config: CheckConfigs = JSON.parse(core.getInput('config'))
+    const action: string = core.getInput('action')
+    const config: CheckConfigs = JSON.parse(core.getInput('config') || '{}')
     const checks: CheckMap = JSON.parse(core.getInput('checks'))
     const token = core.getInput('token')
     const textExtra = core.getInput('text-extra')
-    const requests: Promise<[string, number]>[] = []
     const octokit = github.getOctokit(token)
-    const nwo = process.env['GITHUB_REPOSITORY'] || '/'
-    const [owner, repo] = nwo.split('/')
-    Object.entries(checks).forEach(([check, checkConfig]) => {
-      const name = checkConfig.name
-      const id = check
-      const checkRequestConfig: CheckConfig = checkConfig.action === 'SKIP' ? {...config.skipped} : {...config.run}
-      const output = {...checkRequestConfig.output}
-      output.text = textExtra === '' ? output.text : `${checkRequestConfig.output.text}\n${textExtra}`
-      requests.push(createCheckRun(octokit, {...checkRequestConfig, id, name, owner, repo, output}))
-    })
-    const checkRunIds: [string, number][] = await Promise.all(requests)
-    core.setOutput('checks', JSON.stringify(Object.fromEntries(checkRunIds), null, 0))
+    if (action === 'start') {
+      await startChecks(octokit, checks, config, textExtra)
+    } else if (action === 'update') {
+      await updateChecks(octokit, checks, textExtra)
+    }
   } catch (error) {
     const e = error as Record<'stderr', string>
     if (e.stderr) {
