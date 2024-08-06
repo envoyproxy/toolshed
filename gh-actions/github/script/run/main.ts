aaa3e681e97efa651433fd641a686ddc6e23f31e
phlax <phlax@users.noreply.github.com>
2024-08-06 11:06:41 +0100

    `gh-actions/github/script/run`: Add action (#2161)
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ gh-actions/github/script/run/main.ts
@@ -0,0 +1,48 @@
+import * as core from '@actions/core'
+import {spawn} from 'child_process'
+import * as fs from 'fs'
+import * as path from 'path'
+import {EOL} from 'os'
+
+const script = async (cmd: string): Promise<void> => {
+  try {
+    const subprocess = spawn(cmd, {stdio: 'inherit', shell: true})
+    subprocess.on('exit', (exitCode: number) => {
+      process.exitCode = exitCode
+    })
+  } catch (error) {
+    const e = error as Record<'stderr', string>
+    if (e.stderr) {
+      console.error(`script/run stderr: ${e.stderr}`)
+    }
+    if (error instanceof Error) {
+      console.error(error.message)
+    }
+    core.setFailed(`script/run failure: ${error}`)
+  }
+}
+
+const run = async (): Promise<void> => {
+  if (process.env.INPUT_KEY) {
+    const key = process.env.INPUT_KEY.toUpperCase()
+
+    if (process.env[`STATE_${key}`] !== undefined) {
+      if (process.env.INPUT_POST) {
+        await script(process.env.INPUT_POST)
+      }
+    } else {
+      if (process.env.GITHUB_STATE && process.env.INPUT_MAIN) {
+        const filePath = path.join(__dirname, process.env.GITHUB_STATE)
+        fs.appendFileSync(filePath, `${key}=true${EOL}`)
+        await script(process.env.INPUT_MAIN)
+      }
+    }
+  }
+}
+
+// Don't auto-execute in the test environment
+if (process.env['NODE_ENV'] !== 'test') {
+  run()
+}
+
+export default run
