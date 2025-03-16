15b77cc98171a7034f0f2766375678a4ca179e05
phlax <phlax@users.noreply.github.com>
2025-03-16 15:49:03 +0000

    rust/test: Test the tests (#2600)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- rust/test/src/spy.rs
+++ rust/test/src/spy.rs
@@ -3,7 +3,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Spy {
     pub calls: Arc<Mutex<HashMap<String, Vec<String>>>>,
 }
@@ -15,17 +15,17 @@ impl Spy {
         }
     }
 
+    pub fn clear(&self, key: &str) {
+        let mut calls = self.calls.lock().unwrap();
+        calls.insert(key.to_string(), Vec::new());
+    }
+
     pub fn get(&self, key: &str) -> Vec<String> {
         let mut calls = self.calls.lock().unwrap();
         calls
             .entry(key.to_string())
             .or_insert_with(Vec::new)
             .to_vec()
-    }
-
-    pub fn clear(&self, key: &str) {
-        let mut calls = self.calls.lock().unwrap();
-        calls.insert(key.to_string(), Vec::new());
     }
 
     pub fn push(&self, key: &str, value: &str) {
@@ -36,5 +36,70 @@ impl Spy {
             .to_vec();
         let vec = calls.get_mut(key).unwrap();
         vec.push(value.to_string());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serial_test::serial;
+
+    #[test]
+    #[serial(toolshed_lock)]
+    fn test_spy_constructor() {
+        let spy = Spy::new();
+        let calls = spy.calls.lock().unwrap();
+        assert!(calls.is_empty());
+    }
+
+    #[test]
+    #[serial(toolshed_lock)]
+    fn test_spy_clear() {
+        let spy = Spy {
+            calls: Arc::new(Mutex::new(HashMap::new())),
+        };
+        let messages = vec![
+            "SOME".to_string(),
+            "TEST".to_string(),
+            "MESSAGES".to_string(),
+        ];
+        {
+            let mut calls = spy.calls.lock().unwrap();
+            calls.insert("FOO".to_string(), messages.clone());
+        }
+        spy.clear("FOO");
+        assert!(spy.get("FOO").is_empty());
+    }
+
+    #[test]
+    #[serial(toolshed_lock)]
+    fn test_spy_get() {
+        let spy = Spy {
+            calls: Arc::new(Mutex::new(HashMap::new())),
+        };
+        let messages = vec![
+            "SOME".to_string(),
+            "TEST".to_string(),
+            "MESSAGES".to_string(),
+        ];
+        {
+            let mut calls = spy.calls.lock().unwrap();
+            calls.insert("FOO".to_string(), messages.clone());
+        }
+        assert_eq!(spy.get("FOO"), messages);
+        assert!(spy.get("BAR").is_empty());
+    }
+
+    #[test]
+    #[serial(toolshed_lock)]
+    fn test_spy_push() {
+        let spy = Spy {
+            calls: Arc::new(Mutex::new(HashMap::new())),
+        };
+        let messages = vec!["SOME", "TEST", "MESSAGES"];
+        spy.push("FOO", messages[0]);
+        spy.push("FOO", messages[1]);
+        spy.push("FOO", messages[2]);
+        assert_eq!(spy.get("FOO"), messages);
     }
 }
