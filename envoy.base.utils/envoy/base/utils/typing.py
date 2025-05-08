4cdd3f030aeec67981af82e501915d32e2de6450
Rohit Agrawal <rohit.agrawal@databricks.com>
2025-05-08 05:10:40 -0700

    feat: add PR link support to changelog schema
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- envoy.base.utils/envoy/base/utils/typing.py
+++ envoy.base.utils/envoy/base/utils/typing.py
@@ -1,6 +1,5 @@
-
 import pathlib
-from typing import TypedDict
+from typing import TypedDict, NotRequired, List
 
 from packaging import version as _version
 
@@ -16,11 +15,13 @@ class Change(str):
 class SourceChangeDict(TypedDict):
     area: str
     change: str
+    prs: NotRequired[List[str]]  # Optional PR references
 
 
 class ChangeDict(TypedDict):
     area: str
     change: Change
+    prs: NotRequired[List[str]]  # Optional PR references
 
 
 ChangeList = list[ChangeDict]
