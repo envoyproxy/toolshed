329526a21bfae7fd09da5ca2bbcbfe6cedd5986d
Ryan Northey <ryan@synca.io>
2025-12-22 13:05:11 +0000

    `gh-actions/github/release`: Add debugging and fix
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- gh-actions/github/release/release.sh
+++ gh-actions/github/release/release.sh
@@ -3,17 +3,74 @@
 set -e -o pipefail
 
 # Expected environment variables:
-# - RELEASE_ARGS: arguments for gh release create
+# - DEBUG: echo args as executed
+# - SHA: the release sha
 # - TAG: the release tag
+# - TITLE: the release title
 # - REPO: the repository
 # - FAIL_IF_EXISTS: whether to fail if release exists
 # - DRY_RUN: whether this is a dry run
 # - NEXT_VERSION: next version to write to version file
+# - VERSION: the release version
 # - VERSION_FILE: path to version file
 # - REOPEN_MESSAGE: commit message for version file update
 
-read -ra RELEASE_ARGS_ARR <<< "$RELEASE_ARGS"
+cleanup() {
+    local exit_code=$?
+    cat "$TMP_OUTPUT"
+    rm -f "$TMP_OUTPUT"
+    rm -f "$CMD_OUTPUT"
+    exit $exit_code
+}
 
+trap cleanup EXIT
+
+RELEASE_ARGS=(
+    --target "$SHA"
+    --title "$TAG"
+    --notes "${TITLE} release ${VERSION}"
+    --repo "$REPO"
+    "${TAG}")
+TMP_OUTPUT="$(mktemp)"
+CMD_OUTPUT="$(mktemp)"
+
+if [[ -n "$DEBUG" ]]; then
+    gh version >&2
+fi
+
+print_cmd() {
+    local arg
+    for arg in "$@"; do
+        if [[ "$arg" =~ [[:space:]] ]]; then
+            printf '"%s" ' "$arg"
+        else
+            printf '%s ' "$arg"
+        fi
+    done
+    echo
+}
+
+_run () {
+    echo "$ $(print_cmd "$@")" >> "$TMP_OUTPUT"
+    if [[ -n "$DEBUG" ]]; then
+        echo "$ $(print_cmd "$@")" >&2
+    fi
+    if [[ "${DRY_RUN}" == "true" && ("$*" =~ "gh release create" || "$*" =~ "git push") ]]; then
+        echo "SKIPPED" >> "$TMP_OUTPUT"
+        return
+    fi
+    EXIT_CODE=
+    "$@" 2>&1 | tee -a bar >&2 > "$CMD_OUTPUT" || {
+        EXIT_CODE="$?"
+    }
+    if [[ -n "$DEBUG" ]]; then
+        cat "$CMD_OUTPUT" >&2
+    fi
+    cat "$CMD_OUTPUT" >> "$TMP_OUTPUT"
+    if [[ -n "$EXIT_CODE" ]]; then
+        return "$EXIT_CODE"
+    fi
+}
 
 if gh release view "$TAG" --repo "$REPO" &>/dev/null; then
     if [[ "${FAIL_IF_EXISTS}" == "true" ]]; then
@@ -21,27 +78,17 @@ if gh release view "$TAG" --repo "$REPO" &>/dev/null; then
         exit 1
     fi
     echo "::warning::Release $TAG already exists, skipping creation" >&2
-    OUTPUT=(
-        "$ gh release view \"$TAG\" --repo \"$REPO\""
-        "Release $TAG already exists, skipping creation")
-    _OUTPUT=$(printf '%s\n' "${OUTPUT[@]}")
-    echo "${_OUTPUT}"
+    echo "$ gh release view \"$TAG\" --repo \"$REPO\"" >> "$TMP_OUTPUT"
+    echo "Release $TAG already exists, skipping creation" >> "$TMP_OUTPUT"
     exit 0
 fi
-OUTPUT=("$ gh release create ${RELEASE_ARGS_ARR[*]}")
-if [[ "${DRY_RUN}" == "true" ]]; then
-    OUTPUT+=("SKIPPED")
-else
-    OUTPUT+=("$(gh release create "${RELEASE_ARGS_ARR[@]}")")
+
+_run gh release create "${RELEASE_ARGS[@]}"
+echo "$ echo ${NEXT_VERSION} > ${VERSION_FILE}" >> "$TMP_OUTPUT"
+if [[ -n "$DEBUG" ]]; then
+    echo "$ echo ${NEXT_VERSION} > ${VERSION_FILE}" >&2
 fi
 echo "${NEXT_VERSION}" > "${VERSION_FILE}"
-OUTPUT+=("$ git commit ${VERSION_FILE} -m \"${REOPEN_MESSAGE}\" --signoff")
-OUTPUT+=("$(git commit "${VERSION_FILE}" -m "${REOPEN_MESSAGE}" --signoff)")
-OUTPUT+=("$ git push origin refs/heads/main")
-if [[ "${DRY_RUN}" == "true" ]]; then
-    OUTPUT+=("SKIPPED")
-else
-    OUTPUT+=("$(git push origin refs/heads/main)")
-fi
-_OUTPUT=$(printf '%s\n' "${OUTPUT[@]}")
-echo "${_OUTPUT}"
+_run git commit "${VERSION_FILE}" -m "${REOPEN_MESSAGE}" --signoff
+_run git show
+_run git push origin refs/heads/main
