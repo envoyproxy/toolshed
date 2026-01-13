632ee727b1623b850907bdfd8b865c05d2099483
Ryan Northey <ryan@synca.io>
2026-01-13 09:32:49 +0000

    ci/bazel: Add script to prepare dev push
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ .github/workflows/prepare_bazel_dev.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e -opipefail
+
+MODULE_BAZEL="bazel/MODULE.bazel"
+echo "\$ sed -i '/^module(/,/^)/s/version = \\\"[^\\\"]*\\\"/version = \\\"${NEXT_VERSION}\\\"/' ${MODULE_BAZEL}" >> "$TMP_OUTPUT"
+if [[ -n "$DEBUG" ]]; then
+    echo "\$ sed -i '/^module(/,/^)/s/version = \\\"[^\\\"]*\\\"/version = \\\"${NEXT_VERSION}\\\"/' ${MODULE_BAZEL}" >&2
+fi
+if [[ "${DRY_RUN}" != "true" ]]; then
+    sed -i "/^module(/,/^)/s/version = \"[^\"]*\"/version = \"${NEXT_VERSION}\"/" "${MODULE_BAZEL}"
+fi
+echo "${MODULE_BAZEL}"
