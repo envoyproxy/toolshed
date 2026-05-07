a5a1a0bdd6a80a83b8fa2ca744995def3da622b1
Ryan Northey <ryan@synca.io>
2026-05-07 09:35:40 +0000

    python: Use setup.cfg as source of truth for wheel metadata
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- py/pants-toolshed/toolshed_distribution/my_rules.py
+++ py/pants-toolshed/toolshed_distribution/my_rules.py
@@ -13,9 +13,30 @@ from pants.engine.unions import UnionRule
 
 
 from . import PytoolingSetupKwargsRequest
+from toolshed_setup_cfg import (
+    parse_entry_points,
+    parse_extras_require,
+    parse_metadata,
+    parse_options,
+)
 
 
 class PytoolingSetupKwargsResponse:
+
+    # Pants forbids the following kwargs in `provides=setup_py(...)` because
+    # it derives them from the BUILD graph itself:
+    #   data_files, install_requires, namespace_packages,
+    #   package_dir, package_data, packages
+    # See:
+    #   https://www.pantsbuild.org/docs/python-distributions
+    #   pants/docs/docs/python/overview/building-distributions.mdx
+    #
+    # Therefore the corresponding [options]/[options.*] sections of
+    # setup.cfg are intentionally NOT propagated by this plugin and exist
+    # purely as documentation / setuptools-compatibility. They are:
+    #   - [options] install_requires  (use BUILD deps + requirements.in)
+    #   - [options] packages / py_modules / [options.packages.find]
+    #   - [options.package_data]      (use `resources()` targets)
 
     def __init__(self, request: PytoolingSetupKwargsRequest) -> None:
         self.request = request
@@ -41,23 +62,26 @@ class PytoolingSetupKwargsResponse:
     @property
     def setup_kwargs(self) -> Dict:
         kwargs = self.request.explicit_kwargs.copy()
-        for option in self.config["metadata"]:
-            if option == "long_description":
-                if self.config["metadata"][option].startswith("file:"):
-                    filepath = self.config['metadata'][
-                        option].split(':')[1].strip()
-                    kwargs[option] = pathlib.Path(
-                        f"{self.namespace}/"
-                        f"{filepath}").read_text()
-            elif option != "classifiers":
-                kwargs[option] = self.config["metadata"][option]
+        kwargs.update(parse_metadata(self.config, self.namespace))
         kwargs["version"] = self.version
-        if self.config.has_section("options.entry_points"):
-            for entry_point in self.config["options.entry_points"]:
-                kwargs["entry_points"] = kwargs.get("entry_points", {})
-                kwargs["entry_points"][entry_point] = self.config[
-                    "options.entry_points"][
-                        entry_point].strip().replace(" ", "").split("\n")
+
+        entry_points = parse_entry_points(self.config)
+        if entry_points is not None:
+            merged = kwargs.get("entry_points", {})
+            merged.update(entry_points)
+            kwargs["entry_points"] = merged
+
+        # parse_options returns python_requires, install_requires and
+        # zip_safe. install_requires is forbidden by pants (see comment
+        # at top of class) so strip it here.
+        options_kwargs = parse_options(self.config, self.namespace)
+        options_kwargs.pop("install_requires", None)
+        kwargs.update(options_kwargs)
+
+        extras_require = parse_extras_require(self.config)
+        if extras_require is not None:
+            kwargs["extras_require"] = extras_require
+
         return kwargs
 
     @property
