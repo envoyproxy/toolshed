39fde6f967fda7f0756638e61ed1a6ee92548414
Ryan Northey <ryan@synca.io>
2026-05-10 11:02:40 +0000

    python: Deprecate `envoy.gpg.identity` package
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- py/envoy.gpg.identity/envoy/gpg/identity/__init__.py
+++ py/envoy.gpg.identity/envoy/gpg/identity/__init__.py
@@ -1,7 +1,19 @@
 
+import warnings
+
 from .identity import (
     GPGError,
     GPGIdentity)
+
+DEPRECATION_MESSAGE = (
+    "envoy.gpg.identity is deprecated and will no longer be published as a "
+    "standalone distribution. Its functionality now ships as part of "
+    "envoy.gpg.sign; depend on that instead.")
+
+warnings.warn(
+    DEPRECATION_MESSAGE,
+    DeprecationWarning,
+    stacklevel=2)
 
 
 __all__ = (
